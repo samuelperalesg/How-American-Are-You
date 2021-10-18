caea862042db3da5f1aa5b0a80127e7bf9e452f1
s.pera6@gmail.com <s.pera6@gmail.com>
2021-10-18 11:06:57 -0600

    add buttons to html, add cached element ref, add event listeners

--- js/app.js
+++ js/app.js
@@ -8,10 +8,18 @@
 
 /*------------------------ Cached Element References ------------------------*/
 // Cached elements for User's Game Options
+const messageEl = document.getElementById('welcome')
+const gameOption1 = document.getElementById('option1')
+const gameOption2 = document.getElementById('option2')
+const gameOption3 = document.getElementById('option3')
+const gameOption4 = document.getElementById('option4')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+// gameOption1.addEventListener('click', 'add link to next html')
+gameOption2.addEventListener('click', '')
+gameOption3.addEventListener('click', '')
+gameOption4.addEventListener('click', '')
 
 
 /*-------------------------------- Functions --------------------------------*/
