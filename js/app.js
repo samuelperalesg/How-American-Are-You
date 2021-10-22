7cfe96193d06746b3daaa5b084c9a321cabc37b3
s.pera6@gmail.com <s.pera6@gmail.com>
2021-10-22 07:30:26 -0600

    removed unnessassary code

--- js/app.js
+++ js/app.js
@@ -1,105 +1,108 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const SCORE_POINTS = 10
+const MAX_QUESTIONS = 10
 
 /*-------------------------------- Variables --------------------------------*/
 let currentQuestion = {}
-let chooseOption = true
+let acceptingAnswers = true
 let score = 0
-let questionNumber = 0
-let questionsAvailable = []
+let questionCounter = 0
+let availableQuestions = []
 
-let allQuestions = [
+let questions = [
   { question: 'What is 2+2?',
     choice1: '2',
     choice2: '3',
     choice3: '4',
     choice4: '5',
-    answer: 3
+    answer: 3,
   },
   { question: 'What is 2+5?',
     choice1: '2',
     choice2: '7',
     choice3: '4',
     choice4: '5',
-    answer: 2
+    answer: 2,
   },
   { question: 'What is 2+8?',
     choice1: '10',
     choice2: '3',
     choice3: '4',
     choice4: '5',
-    answer: 1
+    answer: 1,
   },
   { question: 'What is 2+6?',
     choice1: '2',
     choice2: '3',
     choice3: '4',
     choice4: '8',
-    answer: 4
-  }
-]
-const pointsScore = 1
-const maxQuestions = 4
+    answer: 4,
+  }]
 
 /*------------------------ Cached Element References ------------------------*/
 // Cached elements for User's Game Options
-const gameHeadingEl = document.querySelector('.game-heading')
-const questionEl = document.querySelector('#question-text')
-const correctAnsEl = document.querySelector('#correct-answer-text')
-const scoreEl = document.querySelector('#score-text')
-const choices = Array.from(document.querySelectorAll('.choicecontainer'))
-const nextEl = document.querySelector('#next-btn')
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-// gameOption2.addEventListener('click', '')
-// gameOption3.addEventListener('click', '')
-// gameOption4.addEventListener('click', '')
-
+const question = document.querySelector('#question')
+const choices = Array.from(document.querySelectorAll('.choice-text'))
+const progressText = document.querySelector('#progressText')
+const scoreText = document.querySelector('#score')
+const progressBarFull = document.querySelector('#progressBarFull')
 
 /*-------------------------------- Functions --------------------------------*/
-init ()
-
-function init(){
-  questionNumber = 0
+function startGame() {
+  questionCounter = 0
   score = 0
-  questionsAvailable = [...allQuestions]
-  newQuestions()
+  availableQuestions = [...questions]
+  getNewQuestion()
 }
 
-function newQuestions(){
-  questionNumber++
-  const questionIndex = Math.floor(Math.random() * questionsAvailable.length)
-  currentQuestion = questionsAvailable[questionIndex]
-  questionEl.innerText = currentQuestion.question
+function getNewQuestion() {
+  if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
+    localStorage.setItem('mostRecentScore', score)
+
+    return window.location.assign('/end.html')
+  }
+  questionCounter++
+  progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+
+  const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+  currentQuestion = availableQuestions[questionsIndex]
+  question.innerText = currentQuestion.question
 
   choices.forEach(choice => {
     const number = choice.dataset['number']
     choice.innerText = currentQuestion['choice' + number]
   })
 
-  questionsAvailable.splice(questionIndex, 1)
-  chooseOption = true 
+  availableQuestions.splice(questionsIndex, 1)
+  acceptingAnswers = true 
 }
 
 choices.forEach(choice => {
   choice.addEventListener('click', e => {
-    if (!chooseOption) return
+    if (!acceptingAnswers) return
 
-    chooseOption = false
-    const chosenChoice = e.target
-    const chosenAnswer = chosenChoice.dataset['number']
+    acceptingAnswers = false
+    const selectedChoice = e.target
+    const selectedAnswer = selectedChoice.dataset['number']
 
-    let applyClass = chosenAnswer == currentQuestion.answer ? 'correct'  : 'incorrect'
+    let classToApply = selectedAnswer == currentQuestion.answer ? 'correct'  : 'incorrect'
 
-    if(applyClass = 'correct') {
-      raisePoints(pointsScore)
+    if(classToApply === 'correct') {
+      incrementScore(SCORE_POINTS)
     }
 
-    chosenChoice.parentElement.classList.add(applyClass)
+    selectedChoice.parentElement.classList.add(classToApply)
 
-    
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply)
+      getNewQuestion()
+    }, 1000)
   })
 })
+
+incrementScore = num => {
+  score += num
+  scoreText.innerText = score
+}
+
+startGame()
