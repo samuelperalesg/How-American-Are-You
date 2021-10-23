84df3d701219b44ac92e70d58c2317539205b411
s.pera6@gmail.com <s.pera6@gmail.com>
2021-10-23 14:23:17 -0600

    fix code

--- js/app.js
+++ js/app.js
@@ -106,7 +106,6 @@ function getNewQuestion() {
   }
   questionCounter++
   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
-  progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
   const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
   currentQuestion = availableQuestions[questionsIndex]
